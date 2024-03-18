bd46ec8546c08343009cdfbddf40ca4f9f5bce15
HM Varshney <himank.varshney9@gmail.com>
2024-03-18 18:52:25 +0000

    feat: Add setup.py

---
+++ README.md
@@ -0,0 +1,1 @@
+## RC RVC Pipeline
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rc_rvc_pipeline',
+    version='0.1',
+    description='tts-pipeline',
+    author='HM Varshney',
+    packages=find_packages(),
+)
