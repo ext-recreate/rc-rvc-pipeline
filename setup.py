759fe1154130497bcc9c49d220f9f212ad09738c
HM Varshney <himank.varshney9@gmail.com>
2024-03-18 18:54:46 +0000

    update project name in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='rc_rvc_pipeline',
+    name='rvc_pipeline',
     version='0.1',
     description='tts-pipeline',
     author='HM Varshney',
